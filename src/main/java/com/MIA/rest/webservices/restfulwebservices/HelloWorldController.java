ac2e4b58ff2146ae50505b2908272c9c5623d90f
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-27 22:02:36 +0300

    Creating a Hello World Service

---
+++ src/main/java/com/MIA/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.MIA.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+        @GetMapping(path="/hello-world")
+        public String helloWorld(){
+        return  "Hello World";
+        }
+
+}
