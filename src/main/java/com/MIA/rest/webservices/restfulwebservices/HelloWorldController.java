78d4636ce5a26b209d80e8266a026cf0f25225ff
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-28 23:54:39 +0300

    Connecting Angular Frontend with Restful API

--- src/main/java/com/MIA/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/MIA/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.MIA.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
@@ -16,7 +18,7 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
-        return new HelloWorldBean("Hello World");
+        return new HelloWorldBean("Hello World - changed");
     }
 
     @GetMapping(path = "/hello-world/path-variable/{name}")
