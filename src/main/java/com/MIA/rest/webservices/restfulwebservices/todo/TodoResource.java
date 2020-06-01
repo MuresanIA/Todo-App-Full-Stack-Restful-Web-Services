9fe97615325ba3aa2187bdc142bcfdae2b49a105
MuresanIA <muresan.alexandru933@gmail.com>
2020-06-01 15:32:20 +0300

    Connecting Angular Frontend with Todo List RESTful Service

--- src/main/java/com/MIA/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/MIA/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -3,12 +3,14 @@ package com.MIA.rest.webservices.restfulwebservices.todo;
 
 import com.MIA.rest.webservices.restfulwebservices.todo.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class TodoResource {
 
