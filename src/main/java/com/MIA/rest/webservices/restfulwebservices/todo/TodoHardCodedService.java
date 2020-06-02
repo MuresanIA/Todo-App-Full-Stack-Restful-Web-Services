af31e7286533254d5aaff53d4de5ddba77c3ac33
MuresanIA <muresan.alexandru933@gmail.com>
2020-06-02 21:07:02 +0300

    Creating REST API for Updating Todo - PUT Request Method

--- src/main/java/com/MIA/rest/webservices/restfulwebservices/todo/TodoHardCodedService.java
+++ src/main/java/com/MIA/rest/webservices/restfulwebservices/todo/TodoHardCodedService.java
@@ -23,7 +23,17 @@ public class TodoHardCodedService {
     public List<Todo> findAll() {
         return todos;
     }
-
+        // If the id of the to-do is -1 we want to insert it in
+    public Todo save(Todo todo){
+        if (todo.getId()== -1){
+            todo.setId(++idCounter);
+            todos.add(todo);
+        }else{
+            deleteById(todo.getId());
+            todos.add(todo);
+        }
+        return todo;
+    }
 
     public Todo deleteById(long id) {
         Todo todo = findById(id);
