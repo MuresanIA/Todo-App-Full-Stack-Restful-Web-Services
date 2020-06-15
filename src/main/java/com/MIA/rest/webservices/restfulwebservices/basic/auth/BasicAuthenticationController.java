08422242a9442545df6642b3b982eddb0c64b938
MuresanIA <muresan.alexandru933@gmail.com>
2020-06-15 19:46:32 +0300

    Create Basic Authentication RESTful Service in Spring Boot

---
+++ src/main/java/com/MIA/rest/webservices/restfulwebservices/basic/auth/BasicAuthenticationController.java
@@ -0,0 +1,18 @@
+package com.MIA.rest.webservices.restfulwebservices.basic.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+public class BasicAuthenticationController {
+
+    @GetMapping(path = "/basicauth")
+    public AuthenticationBean helloWorldBean() {
+//        throw new RuntimeException("Some Error Has Happened! Contact Support at ***-***");
+        return new AuthenticationBean("You are authenticated");
+    }
+
+
+}
