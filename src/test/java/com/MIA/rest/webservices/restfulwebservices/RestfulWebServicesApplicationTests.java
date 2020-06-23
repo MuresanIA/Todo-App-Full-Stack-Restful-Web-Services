510636b9cc35f5ed5f06028a449078a611f1338b
MuresanIA <muresan.alexandru933@gmail.com>
2020-06-23 19:39:26 +0300

    Changed to 2.3.0.Release.

--- src/test/java/com/MIA/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/MIA/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,6 +1,6 @@
 package com.MIA.rest.webservices.restfulwebservices;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
