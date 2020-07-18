e44aece317f2f75a622e292dc2a6c789cbaa673a
MuresanIA <muresan.alexandru933@gmail.com>
2020-07-18 14:58:17 +0300

    works?

--- src/main/java/com/MIA/rest/webservices/restfulwebservices/jwt/JWTWebSecurityConfig.java
+++ src/main/java/com/MIA/rest/webservices/restfulwebservices/jwt/JWTWebSecurityConfig.java
@@ -53,7 +53,9 @@ public class JWTWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable().exceptionHandling()
+
+        httpSecurity.cors().and()
+                .csrf().disable().exceptionHandling()
                 .authenticationEntryPoint(jwtUnAuthorizedResponseAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests().anyRequest()
                 .authenticated();
